66055083b6b9ab7b3085a9faea45cb5467f03f43
Khushi Upadhyay <106996372+khuzii@users.noreply.github.com>
2022-06-08 00:43:12 +0530

    Create Q3 duplicatenos.cpp

---
+++ Week 3/Q3 duplicatenos.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void MergeSortedIntervals(vector<int> &v, int s, int m, int e)
+{
+	vector<int> temp;
+
+	int i, j;
+	i = s;
+	j = m + 1;
+
+	while (i <= m && j <= e)
+	{
+
+		if (v[i] <= v[j])
+		{
+			temp.push_back(v[i]);
+			++i;
+		}
+		else
+		{
+			temp.push_back(v[j]);
+			++j;
+		}
+	}
+
+	while (i <= m)
+	{
+		temp.push_back(v[i]);
+		++i;
+	}
+
+	while (j <= e)
+	{
+		temp.push_back(v[j]);
+		++j;
+	}
+
+	for (int i = s; i <= e; ++i)
+		v[i] = temp[i - s];
+}
+
+void MergeSort(vector<int> &v, int s, int e)
+{
+	if (s < e)
+	{
+		int m = (s + e) / 2;
+		MergeSort(v, s, m);
+		MergeSort(v, m + 1, e);
+		MergeSortedIntervals(v, s, m, e);
+	}
+}
+int main()
+{
+	int t, n, temp, f = 0;
+	vector<int> v;
+	cin >> t;
+	while (t)
+	{
+		f = 0;
+		cin >> n;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> temp;
+			v.push_back(temp);
+		}
+		MergeSort(v, 0, n - 1);
+		for (int i = 0; i < n; i++)
+		{
+			if (v[i] == v[i + 1])
+			{
+				cout << "YES" << endl;
+				f = 1;
+				break;
+			}
+		}
+		if (f == 0)
+			cout << "NO" << endl;
+		v.clear();
+		t--;
+	}
+}
